8e55754dbc27d6c3da13e342bce6dea5af0c2b24
Cássio Costa <cassiocosta.wk@gmail.com>
2026-03-21 20:58:11 -0300

    feat(application): add commands and results for Sale and SaleItem

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/Common/SaleItemResult.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Common
+{
+    /// <summary>
+    /// Response model for SaleItem.
+    /// </summary>
+    public class SaleItemResult
+    {
+        /// <summary>
+        /// The unique identifier of the sale item.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The unique identifier of the sale associated with this item.
+        /// </summary>
+        public Guid SaleId { get; set; }
+
+        /// <summary>
+        /// The quantity of the item in the sale.
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// The unit price of the item in the sale.
+        /// </summary>
+        public decimal UnitPrice { get; set; }
+
+        /// <summary>
+        /// The discount applied to the item in the sale.
+        /// </summary>
+        public decimal Discount { get; set; }
+
+        /// <summary>
+        /// The total amount of the item in the sale.
+        /// </summary>
+        public decimal TotalAmount { get; set; }
+
+        /// <summary>
+        /// Indicates whether the item in the sale is cancelled.
+        /// </summary>
+        public bool IsCancelled { get; set; }
+
+        /// <summary>
+        /// The unique identifier of the product associated with this item.
+        /// </summary>
+        public int ProductId { get; set; }
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/Common/UpdateSaleItemCommand.cs
@@ -0,0 +1,50 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Sales.Common
+{
+    /// <summary>
+    /// DTO for updating items on a sale.
+    /// </summary>
+    /// <remarks>
+    /// This DTO is used to transfer data when updating items in a sale.
+    /// </remarks>
+    public class UpdateSaleItemCommand
+    {
+        /// <summary>
+        /// The unique identifier of the sale item being updated. 
+        /// </summary>
+        /// <remarks>
+        /// Optional for new items being added to the sale, 
+        /// but required for existing items that are being updated.
+        /// </remarks>
+        public Guid? Id { get; set; }
+
+        /// <summary>
+        /// The quantity of the item in the sale.
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// The unit price of the item in the sale.
+        /// </summary>
+        public decimal UnitPrice { get; set; }
+
+        /// <summary>
+        /// The discount applied to the item in the sale.
+        /// </summary>
+        public decimal Discount { get; set; }
+
+        /// <summary>
+        /// The total amount of the item in the sale.
+        /// </summary>
+        public decimal TotalAmount { get; set; }
+
+        /// <summary>
+        /// Indicates whether the item in the sale is cancelled.
+        /// </summary>
+        public bool IsCancelled { get; set; }
+
+        /// <summary>
+        /// The unique identifier of the product associated with this item.
+        /// </summary>
+        public int ProductId { get; set; }
+    }
+}
