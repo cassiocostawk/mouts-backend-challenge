236a2668c6de51ceb8ec357c057c62a81f947ec0
Cássio Costa <cassiocosta.wk@gmail.com>
2026-03-21 21:45:12 -0300

    feat(mapper): add AutoMapper profiles for sales

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/ListSales/ListSalesProfile.cs
@@ -0,0 +1,20 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.ListSales;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.ListSales
+{
+    /// <summary>
+    /// Profile for mapping between Application and API ListSales requests and responses
+    /// </summary>
+    public class ListSalesProfile : Profile
+    {
+        /// <summary>
+        /// Initializes the mappings for ListSales feature
+        /// </summary>
+        public ListSalesProfile()
+        {
+            CreateMap<ListSalesRequest, ListSaleCommand>();
+            CreateMap<ListSaleResult, ListSalesResponse>();
+        }
+    }
+}
